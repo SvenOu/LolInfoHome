696df491cf4ae4ce8f4ba0d2c1c89f42dfaec01e
SvenOu <948077475@qq.com>
2017-12-27 10:52:02 +0800

    fix bug

--- LolInfoServer/src/main/java/com/svenou/myapp/ServerProfileLoader.java
+++ LolInfoServer/src/main/java/com/svenou/myapp/ServerProfileLoader.java
@@ -11,6 +11,12 @@ import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
+/*
+* @fixme
+* 加入了gradle buildConfig插件，
+* generateBuildConfig task必须设置成 execute after sync 和 execute after make
+* (否则,每次打包的时候就需要手动跑一次 generateBuildConfig task)
+* */
 @Configuration
 public class ServerProfileLoader {
 
